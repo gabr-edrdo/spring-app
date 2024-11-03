65a5f947265a928e64627061f9230359e0fa7365
Gabriel Araujo <gabr.edrdo@gmail.com>
2024-11-02 21:14:12 -0300

    Add criação e configuração do banco sqlite (conforme https://www.baeldung.com/spring-boot-sqlite) e o model Pessoa

---
+++ src/main/java/com/gabr/edrdo/spring_app/model/Pessoa.java
@@ -0,0 +1,49 @@
+package com.gabr.edrdo.spring_app.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "pessoas")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "cpf", nullable = false, unique = true)
+    private String cpf;
+
+    @Column(name = "cep", nullable = false)
+    private String cep;
+
+    @Column(name = "logradouro")
+    private String logradouro;
+
+    @Column(name = "numero")
+    private String numero;
+
+    @Column(name = "complemento")
+    private String complemento;
+
+    @Column(name = "bairro")
+    private String bairro;
+
+    @Column(name = "cidade")
+    private String cidade;
+
+    @Column(name = "estado")
+    private String estado;
+}
